4733aa2fe4dbf3549796250e900ec2cbc094f6f7
Nelson Barrantes <nelson.atle@gmail.com>
2021-09-03 11:13:44 -0600

    Updated the way in which a navigation route is sent in google maps

--- index.js
+++ index.js
@@ -1,50 +1,37 @@
 import { Linking } from 'react-native'
 
-const isValidLatLng = (num, range) => typeof num === 'number' && num <= range && num >= -1 * range
-
-const isValidCoordinates = coords =>
-  isValidLatLng(coords.latitude, 90) && isValidLatLng(coords.longitude, 180)
 
 const getParams = (params = []) => {
-  return params
+  params = params
     .map(({ key, value }) => {
-      const encodedKey = encodeURIComponent(key)
-      const encodedValue = encodeURIComponent(value)
-      return `${encodedKey}=${encodedValue}`
+      return `${value}`
     })
-    .join('&')
+    .join('/')
+  
+  return params
 }
 
-const getWaypoints = (waypoints = []) => {
-  if (waypoints.length === 0) {
-    return ''
-  }
 
-  const params = waypoints
-    .map(value => `${value.latitude},${value.longitude}`)
-    .join('|')
-
-  return `&waypoints=${params}`
-}
-
-function getDirections ({ destination, source, params = [], waypoints = [] } = {}) {
-  if (destination && isValidCoordinates(destination)) {
-    params.push({
-      key: 'destination',
-      value: `${destination.latitude},${destination.longitude}`
-    })
-  }
-
-  if (source && isValidCoordinates(source)) {
+function getDirections({ destination, source, params = [], waypoints = [] } = {}) {
+  if (source) {
     params.push({
       key: 'origin',
-      value: `${source.latitude},${source.longitude}`
+      value: `${source}`
     })
   }
 
-  const url = `https://www.google.com/maps/dir/?api=1&${getParams(
+  if (destination) {
+    params.push({
+      key: 'destination',
+      value: `${destination}`
+    })
+  }
+
+  var url = `https://www.google.com/maps/dir/${getParams(
     params
-  )}${getWaypoints(waypoints)}`
+  )}`
+  url = url.replace('driving/navigate/', '')
+  
   return Linking.canOpenURL(url).then(supported => {
     if (!supported) {
       return Promise.reject(new Error(`Could not open the url: ${url}`))
