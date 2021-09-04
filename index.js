8ceeacf23b826c4fff74f9e368585498feda4cd0
Nelson Barrantes <nelson.atle@gmail.com>
2021-09-03 20:14:53 -0600

    Fix add stops

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+// Fix for Google  Map DIrection IOS
 import { Linking } from 'react-native'
+const isValidLatLng = (num, range) => typeof num === 'number' && num <= range && num >= -1 * range
 
+const isValidCoordinates = coords =>
+  isValidLatLng(coords.latitude, 90) && isValidLatLng(coords.longitude, 180)
 
-const getParams = (params = []) => {
-  params = params
-    .map(({ key, value }) => {
-      return `${value}`
-    })
+// const getParams = (params = []) => {
+//   return params
+//     .map(({ key, value }) => {
+//       const encodedKey = encodeURIComponent(key)
+//       const encodedValue = encodeURIComponent(value)
+//       return `${encodedKey}=${encodedValue}`
+//     })
+//     .join('&')
+// }
+
+const getParams = (params = []) => { return params .map(({ key, value }) => { 
+  const encodedKey = encodeURIComponent(key) 
+  const encodedValue = encodeURIComponent(value) 
+  return `${value}`}) .join('/') }
+const getWaypoints = (waypoints = []) => {
+  if (waypoints.length === 0) {
+    return ''
+  }
+
+  const params = waypoints
+    .map(value => `${value.latitude},${value.longitude}`)
     .join('/')
-  
-  return params
+
+  return `/${params}`
 }
 
-
-function getDirections({ destination, source, params = [], waypoints = [] } = {}) {
+function getDirections ({ destination, source, params = [], waypoints = [] } = {}) {
   if (source) {
     params.push({
       key: 'origin',
@@ -27,11 +46,14 @@ function getDirections({ destination, source, params = [], waypoints = [] } = {}
     })
   }
 
+  
+
   var url = `https://www.google.com/maps/dir/${getParams(
     params
-  )}`
+  )}${getWaypoints(waypoints)}`
   url = url.replace('driving/navigate/', '')
-  
+
+  console.log("url to send", url);
   return Linking.canOpenURL(url).then(supported => {
     if (!supported) {
       return Promise.reject(new Error(`Could not open the url: ${url}`))
